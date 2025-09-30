e6b66e23b626d0d514b5fde2731f286b39917be2
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-10-01 03:55:15 +0900

    Initial commit

---
+++ vite-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  // この行を追加することで ImportMetaEnv の型を厳密にし、不明なキーを許可しないように
+  // できます。
+  strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+  readonly VITE_CONVEX_URL: string
+  readonly VITE_CONVEX_DEPLOYMENT: string
+  // その他の環境変数...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
